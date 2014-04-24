b7d0e112ff9d84716189c6a336e8b2cf04af0ed0
Arash <Arash@172.30.33.83>
2014-04-24 15:04:08 -0400

    first creation

---
+++ src/main/java/cloudservices/brokerage/policy/simplebrokertest/App.java
@@ -0,0 +1,13 @@
+package cloudservices.brokerage.policy.simplebrokertest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
